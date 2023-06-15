dd5549001d4787e9a86b8c86f8f27ff5c2cc281c
DPrudnikov <dima1911@mail.ru>
2023-06-15 19:08:06 +0300

    Add Ex 50

---
+++ Ex50/Program.cs
@@ -0,0 +1,65 @@
+﻿// Зад. 50 Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+
+Console.WriteLine("Задайте искомую строку ");
+int findrows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте искоый столбец ");
+int findcolumns = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = new int[6, 5];
+
+void CreatFillArray()
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            newArray[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            Console.Write(newArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElementPosition()
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            if ((i == findrows) && (j == findcolumns))
+            {
+                Console.Write(newArray[findrows, findcolumns]);
+            }
+
+        }
+
+    }
+    Console.WriteLine();
+
+    if ((findrows > newArray.GetLength(0)) || (findcolumns > newArray.GetLength(1)))
+        {
+            Console.Write($"Элемента с заданной позицией{(findrows, findcolumns)} в массиве нет");
+        }
+
+    
+
+}
+
+CreatFillArray();
+PrintArray();
+Console.WriteLine("________________");
+FindElementPosition();
+
