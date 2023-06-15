57a465deecc07bf73dded37a8a56a4e9cd850077
DPrudnikov <dima1911@mail.ru>
2023-06-15 19:12:06 +0300

    Add Ex52

---
+++ Ex52/Program.cs
@@ -0,0 +1,53 @@
+﻿// №52 Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = new int[rows, columns];
+
+void CreatFillArray()
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            newArray[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            Console.Write(newArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageColumns()
+{
+    double sum = 0;
+    for (int j = 0; j < newArray.GetLength(1); j++)
+    {
+
+        for (int i = 0; i < newArray.GetLength(0); i++)
+        {
+            sum = sum + newArray[i, j];
+        }
+        Console.Write(sum/rows + " | ");
+    }
+
+    Console.WriteLine();
+
+}
+
+CreatFillArray();
+PrintArray();
+AverageColumns();
